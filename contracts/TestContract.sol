e4042d6e2cc64776e88d34b268f5ab7042c20bee
Federico Levin <federico.levin@despegar.com>
2021-08-23 21:52:26 -0300

    function modifier checkOdd added

--- contracts/TestContract.sol
+++ contracts/TestContract.sol
@@ -3,7 +3,12 @@ pragma solidity ^0.8.7;
 contract TestContract {
     uint stateVariable = 23;
 
-    function double(uint x) public pure returns (uint) {
+    function double(uint x) public pure checkOdd(x) returns (uint) {
         return x * 2;
+    }
+
+    modifier checkOdd(uint x) {
+        require(x % 2 == 0);
+        _; // acá es donde se va a ejecutar la función "modificada".
     }
 }
