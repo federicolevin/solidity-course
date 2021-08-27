ecdc47d53fe0f34cc0e0796bac7e647e096a24c1
Federico Levin <federico.levin@despegar.com>
2021-08-27 20:27:18 -0300

    added findPosition

--- contracts/TestContract.sol
+++ contracts/TestContract.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.7;
 
 contract TestContract {
     uint stateVariable = 23;
+    uint[] randomNumbers = [2, 5, 10, 25, 65];
 
     function double(uint x) public pure checkOdd(x) returns (uint) {
         return x * 2;
@@ -10,5 +11,14 @@ contract TestContract {
     modifier checkOdd(uint x) {
         require(x % 2 == 0);
         _; // acá es donde se va a ejecutar la función "modificada".
+    }
+
+    function findPosition(uint numberToFind) public view returns (uint) {
+        for (uint i = 0; i < randomNumbers.length; i++) {
+            if (randomNumbers[i] == numberToFind) {
+                return i;
+            }   
+        }
+        return 0;
     }
 }
